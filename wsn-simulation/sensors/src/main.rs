d2510980ecb7672887ade5007cecaa77e8d6f804
RodrigoSdeCarvalho <rodrigosc2401@gmail.com>
2024-05-14 10:19:40 -0300

    Fix

--- wsn-simulation/sensors/src/main.rs
+++ wsn-simulation/sensors/src/main.rs
@@ -1,13 +1,16 @@
 use std::thread;
+use rocket::log::private::info;
 
 use sensors::sensor::{GLOBAL_SAMPLING_RATE, Sensor, get_sensors};
+use system::Logger;
 
 #[tokio::main]
 async fn main() {
     let sensors = get_sensors();
-    println!("{}", sensors.len());
     for sensor in sensors {
-        sensor.sense(String::from("http://127.0.0.1:8000/push_data")).await;
-        println!("Starting sensor: {}", sensor.name);
+        Logger::info(format!("Starting sensor: {}", sensor.name), true);
+        tokio::spawn(async move {
+            sensor.sense(String::from("http://127.0.0.1:8000/push_data")).await;
+        });
     }
 }
